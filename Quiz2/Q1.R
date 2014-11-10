406c2722f89489a4b5594f14d6ba7999351deb94
andyclam <chunyin91@gmail.com>
2014-11-09 19:50:19 -0800

    Initial Upload

---
+++ Quiz2/Q1.R
@@ -0,0 +1,28 @@
+library(httr)
+library(httpuv)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. Register an application at https://github.com/settings/applications;
+#    Use any URL you would like for the homepage URL (http://github.com is fine)
+#    and http://localhost:1410 as the callback url
+#
+#    Insert your client ID and secret below - if secret is omitted, it will
+#    look it up in the GITHUB_CONSUMER_SECRET environmental variable.
+myapp <- oauth_app("github", "42f6c27228114aff52c4", "5358a72e18568cce2811c799448130e89293e3a3")
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/rate_limit", gtoken)
+stop_for_status(req)
+content(req)
+
+# OR:
+req <- with_config(gtoken, GET("https://api.github.com/rate_limit"))
+stop_for_status(req)
+content(req)
